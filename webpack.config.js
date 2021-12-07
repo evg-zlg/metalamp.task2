69b3818504b60353cb66d25e447dbc87eca8be6f
evg-zlg <evg.zalogin@gmail.com>
2021-12-07 17:36:17 +0500

    Added Babel in webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/app/index.js'),
+    },
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        title: 'webpack Boilerplate',
+        template: path.resolve(__dirname, './src/public/index.html'), // шаблон
+        filename: 'index2.html', // название выходного файла
+    })
+],
+}
