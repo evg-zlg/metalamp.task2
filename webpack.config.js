7e4d3731d43596dac88f2830a464f552d57d56d1
evg-zlg <evg.zalogin@gmail.com>
2021-12-07 14:15:11 +0500

    Install Webpack, yarn start

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
+  output: {
+    path: __dirname + '/dist', // Folder to store generated bundle
+    filename: 'bundle.js',  // Name of generated bundle after build
+    publicPath: '/' // public URL of the output directory when referenced in a browse
+  },
+  module: {  // where we defined file patterns and their loaders
+      rules: [ 
+      ]
+  },
+  plugins: [  // Array of plugins to apply to build chunk
+      new HtmlWebpackPlugin({
+          template: __dirname + "/src/public/index.html",
+          inject: 'body'
+      })
+  ],
+  devServer: {  // configuration for webpack-dev-server
+    static: {
+      directory: path.join(__dirname, 'public'), //source of static assets
+    },  
+    compress: true,
+    port: 9000, // port to run dev-server
+  } 
+};
